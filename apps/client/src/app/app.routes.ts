4f3eba1a0a9a9efaa45aa5f14244d4c350d1e676
AnooshRemote <anooshremotework2025@gmail.com>
2025-12-04 00:28:44 +0100

    feat: add routing, navigation, and responsive styling

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./components/product-list/product-list.component').then(
+        (m) => m.ProductListComponent
+      ),
+  },
+  {
+    path: 'products/new',
+    loadComponent: () =>
+      import('./components/product-form/product-form.component').then(
+        (m) => m.ProductFormComponent
+      ),
+  },
+  {
+    path: 'products/:id',
+    loadComponent: () =>
+      import('./components/product-detail/product-detail.component').then(
+        (m) => m.ProductDetailComponent
+      ),
+  },
+  {
+    path: 'products/:id/edit',
+    loadComponent: () =>
+      import('./components/product-form/product-form.component').then(
+        (m) => m.ProductFormComponent
+      ),
+  },
+];
